correct error in quaternion from euler, need to add tests to check

geometry/point.py
@@ -9,7 +9,8 @@ FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with
 this program. If not, see <http://www.gnu.org/licenses/>.
 """
-from math import pi, cos, sin, sqrt, acos
+from math import pi, sqrt, acos
+import numpy as np
 from typing import List
 from numbers import Number
 from typing import List, Union, Dict
@@ -138,11 +139,11 @@ class Point(object):
 
     @property
     def cosines(self):
-        return Point(x=cos(self.x), y=cos(self.y), z=cos(self.z))
+        return Point(x=np.cos(self.x), y=np.cos(self.y), z=np.cos(self.z))
 
     @property
     def sines(self):
-        return Point(x=sin(self.x), y=sin(self.y), z=sin(self.z))
+        return Point(x=np.sin(self.x), y=np.sin(self.y), z=np.sin(self.z))
 
     def rotate(self, rotation_matrix=List[List[float]]):
         return Point(

geometry/quaternion.py
@@ -55,22 +55,32 @@ class Quaternion():
 
     def transform_point(self, point: Point):
         '''Transform a point by the rotation described by self'''
-        return self * Quaternion(0, point) * self.inverse()
+        return (self * Quaternion(0, point) * self.inverse()).axis
 
     @staticmethod
     def from_euler(eul: Point):
+        '''    def euler_to_quaternion(yaw, pitch, roll):
+
+                    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+                    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+                    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+                    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+
+                    return [qx, qy, qz, qw]
+        '''
+
         half = eul * 0.5
         c = half.cosines
         s = half.sines
         return Quaternion(
-            w=c.y * c.z * c.x - s.y * s.z * s.x,
+            w=c.y * c.z * c.x + s.y * s.z * s.x,
             axis=Point(
-                x=s.y * s.z * c.x + c.y * c.z * s.x,
+                x=c.y * c.z * s.x - s.y * s.z * c.x,
                 y=s.y * c.z * c.x + c.y * s.z * s.x,
                 z=c.y * s.z * c.x - s.y * c.z * s.x
             )
         )
-
+            
     def to_euler(self):
         roll = atan2(
             2 * (self.w * self.x + self.y * self.z),

geometry/transformation.py
@@ -8,8 +8,11 @@ class Transformation():
         self.coord_b = coord_b
 
         self.translation = self.coord_b.origin - self.coord_a.origin
+
         self.rotation = np.dot(
             self.coord_b.inverse_rotation_matrix, self.coord_a.rotation_matrix)
+
+        self.quaternion = Quaternion.from_rotation_matrix(self.rotation)
 
         self.pos_vec = np.vectorize(
             lambda *args: self.point(Point(*args)).to_tuple())
@@ -29,4 +32,4 @@ class Transformation():
         return self.rotate(self.translate(point))
 
     def quat(self, quat: Quaternion):
-        return Quaternion(quat.w, self.rotate(quat.axis))
+        return quat * self.quaternion